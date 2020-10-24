0e15803ddfda096e6cba3811486e96b8e99212e8
Eduardo Fernandez <eduardo.fernandez@mercadolibre.com>
2020-10-24 14:12:38 -0300

    add: coupling concepts

--- src/main/oop/coupling/CouplingJavaIssue.java
+++ src/main/oop/coupling/CouplingJavaIssue.java
@@ -1,2 +1,41 @@
-package main.oop.coupling;public class CouplingJavaIssue {
+package main.oop.coupling;
+
+
+// Coupling is a measure of how much a class is dependent on other classes.
+// There should minimal dependencies between classes.
+// Consider the example below:
+
+class ShoppingCartItem {
+    public float price;
+    public int quantity;
 }
+
+class ShoppingCart {
+    public ShoppingCartItem[] items;
+}
+
+class Order {
+    private ShoppingCart cart;
+    private float salesTax;
+
+    public Order(ShoppingCart cart, float salesTax) {
+        this.cart = cart;
+        this.salesTax = salesTax;
+    }
+
+    /*
+     * This method know the internal details of ShoppingCartEntry and
+     * ShoppingCart classes. If there is any change in any of those
+     * classes, this method also needs too change.
+     */
+    public float orderTotalPrice() {
+        float cartTotalPrice = 0;
+        for(ShoppingCartItem item : cart.items){
+            cartTotalPrice+= item.price * item.quantity;
+        }
+        cartTotalPrice += cartTotalPrice * salesTax;
+        return cartTotalPrice;
+    }
+}
+
+public class CouplingJavaIssue { }
--- src/main/oop/coupling/CouplingJavaSolution.java
+++ src/main/oop/coupling/CouplingJavaSolution.java
@@ -1,2 +1,41 @@
-package main.oop.coupling;public class CouplingJavaSolution {
+package main.oop.coupling;
+
+class CartItem {
+    public float price;
+    public int quantity;
+
+    public float getTotalPrice() {
+        return price * quantity;
+    }
+}
+
+class Cart {
+    CartItem[] items;
+
+    public float getTotalPrice(){
+        float totalPrice = 0;
+        for(CartItem item : items){
+            totalPrice+= item.price * item.quantity;
+        }
+        return totalPrice;
+    }
+}
+
+
+class COrder {
+    private Cart cart;
+    private float salesTax;
+
+    public COrder(Cart cart, float salesTax) {
+        this.cart = cart;
+        this.salesTax = salesTax;
+    }
+
+    public float totalPrice() {
+        return cart.getTotalPrice() * (1.0F + salesTax);
+    }
+
+}
+
+public class CouplingJavaSolution {
 }
